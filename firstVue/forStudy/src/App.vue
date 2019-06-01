be5934377309df40073df20b285283a0ab34ccd4
unknown <tan.s@bellesports.com.cn>
2019-06-01 18:11:19 +0800

    学习数据同步

--- firstVue/forStudy/src/App.vue
+++ firstVue/forStudy/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+      <span>玩家数量：</span><input v-model="playerNumber" id="playerNumber"/>
+      <span>每名玩家角色数：</span><input id="chessPerPlayer" :value="chessPerPlayer" />
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App1'
+  name: 'App1',
+  data () {
+    return {
+      playerNumber: 4,
+      chessPerPlayer: 3
+    }
+  }
 }
 </script>
 
--- firstVue/forStudy/src/components/GameContainer.vue
+++ firstVue/forStudy/src/components/GameContainer.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>hello my</div>
+    <div>my Vue
+    </div>
 </template>
 <script>
 export default {
